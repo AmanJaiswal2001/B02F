8642de5af8cb0be27d23d84544f05a3db003bb94
AmanJaiswal2001 <amanjaisawal537>
2024-11-14 09:34:21 +0530

    hero head

--- B02F/src/components/Header.jsx
+++ B02F/src/components/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from "react"
 
 import theAssign from "../assets/theASSignerlogo 2.png"
 function Header() {
+
+    const[isOpen, setisOpen]=useState(false);
+
+const toggleMenu=()=>{
+    setisOpen(!isOpen);
+}
+
+
   return (
     <>
-    <div className='bg-fuchsia-800 h-10' >
-    <div className='flex justify-end top-1 gap-4 pt-1 pr-3 pb-1 pl-3 '>
+    <div className='md:bg-fuchsia-800 md:h-10 md:block   hidden'>
+    <div className='flex justify-end top-1 gap-4 pt-1 pr-3 pb-1 pl-3  '>
     <a className='text-white'><i class="fa-brands fa-linkedin-in"></i></a>
     <a className='text-white'><i class="fa-brands fa-meta"></i></a>
     <a className='text-white'><i class="fa-brands fa-instagram"></i></a>
@@ -14,15 +22,15 @@ function Header() {
     <a className='text-white'><i class="fa-brands fa-telegram"></i></a>
 </div>
     </div>
-<div>
-<div className="flex justify-between items-center shadow-lg h-28 ">
-<div className='w-56 h-20 ml-8 '>
+
+<div className=" w-full mx-auto flex justify-between items-center shadow-lg md:h-28 p-3 md:p-0 overflow-x-hidden ">
+<div className='md:w-56 md:h-20  w-20 h-9 '>
 <img className='top-11'
  src={theAssign} alt="logo"/>
 </div>
 
-<div className='bg-gray-200 h-12 w-[780px] top-20 left-9 shadow-lg rounded-sm '>
-<ul className='flex pt-3 pr-9 pb-3 pl-9 gap-16 cursor-pointer'>
+<div className='hidden md:flex bg-gray-200 h-12 w-[780px] top-20 mx-4 shadow-lg rounded-sm '>
+<ul className='   flex p-3 gap-4 pt-3 pr-9 pb-3 pl-9 md:gap-16 cursor-pointer'>
    <li>Home </li>
    <li>AboutUs </li>
    <li>Blog </li>
@@ -32,17 +40,36 @@ function Header() {
 </ul>
 
 </div>
-<div className='w-32 h-10 top-20  mr-10 rounded-sm bg-violet-800 text-center flex text-white justify-center'>
+<div className='font-cormorant text-[10px]   ml-auto  leading-[12.11px] md:text-[15px] md:leading-[18.17px] font-bold  md:m-0 m-4 w-11 h-6 pt-1 md:pt-0 md:w-32 md:h-10 md:top-20  md:mr-10 rounded-sm bg-violet-800 text-center md:flex text-white md:justify-center'>
     <button>
     Login
     </button>
 </div>
-<div clas>
-<i class="fa-solid fa-bars"></i>
-</div>
-</div>
 
+<div className="md:hidden flex items-center ">
+<button onClick={toggleMenu} className="text-gray-800  w-10 h-10 text-3xl top-9  ">
+          <i className="fa-solid fa-bars"></i>
+        </button></div>
+        
 </div>
+{isOpen && (
+        <div className="   md:hidden  bg-gray-200 shadow-lg rounded-sm w-full   ">
+          <ul className="  flex flex-col items-center p-4 gap-4 text-sm top-20 cursor-pointer">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Blog</li>
+            <li>Our Services</li>
+            <li>IETS & PTE</li>
+            <li>Contact Us</li>
+          
+          </ul>
+        </div>
+      )}
+
+
+
+
+
 </>
   )
 }
--- B02F/src/components/Hero.jsx
+++ B02F/src/components/Hero.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import image1 from "../assets/image 1.png"
 function Hero() {
   return (
-    <div className="relative">
-<img className='md:w-screen md:h-[501px] md:top-0.5 md:left-0.5 sm:w-screen sm:h-[501px] sm:top-0.5 sm:left-0.5 w-80 h-32 top-' 
+    <div className="  relative">
+<img className=' ml-2 w-92 md:w-screen md:h-[501px] md:top-0.5 md:left-0.5 sm:w-screen sm:h-[501px] sm:top-0.5 sm:left-0.5  h-32 rounded-sm' 
 src={image1} alt="hero image"/>
-
-<div className=' absolute top-20 left-5 h-14  w-28 bg-green-300 md:hidden sm:hidden' >
-    <p className='  font-cormorant text-[11px] font-bold  text-left '>Navigate Your</p>
-    <p className='  font-cormorant text-[11px] font-bold  text-left '>Academic Journey</p> 
-    <p className='  font-cormorant text-[11px] font-bold  text-left '>with Confidence</p>
-    
+<div className='absolute   left-5 top-3 md:hidden sm:hidden'>
+<div className='   h-14  w-28  md:hidden sm:hidden' >
+    <p className='  font-cormorant text-[11px]  leading-[13.32px] font-bold text-purple-500 text-left '>Navigate Your</p>
+    <p className='  font-cormorant text-[11px]  leading-[13.32px] font-bold text-purple-900 text-left '>Academic Journey</p> 
+    <p className='  font-cormorant text-[11px]  leading-[13.32px] font-bold text-pink-600 text-left '>with Confidence</p>
+  
 </div>
 
+<div className='w-56  left-5'>
+  <p className='font-cormorant text-[10px] leading-[12.11px]'>Providing high-quality and comprehensive assistance with assignments and thesis writing for students 
+  at all academic levels.</p>
+</div>
+</div>
 
 
     </div>
